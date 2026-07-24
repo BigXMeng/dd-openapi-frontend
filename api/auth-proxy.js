dcc0f7ac9f8bb393eb45d5060fbdad7c6949124d
liuxianmeng <2751187368@qq.com>
2026-07-24 09:12:55 +0800

    【代理】更换网关IP

--- api/auth-proxy.js
+++ api/auth-proxy.js
@@ -1,13 +1,8 @@
 import proxyRequest from './_utils';
 
 // 认证服务代理
-// export default async (req, res) => {
-//   const path = req.url.replace('/auth-api/', '/gateway-api/auth-service/');
-//   const target = `http://1.14.106.222:10088${path}`;
-//   await proxyRequest(req, res, target);
-// };
 export default async (req, res) => {
-  const path = req.url.replace('/auth-api/', '/auth-service/');
-  const target = `http://1.14.106.222:10066${path}`;
+  const path = req.url.replace('/auth-api/', '/gateway-api/auth-service/');
+  const target = `http://1.14.106.222:10088${path}`;
   await proxyRequest(req, res, target);
 };
